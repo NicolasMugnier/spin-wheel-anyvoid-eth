a294b3d059ac1b2f7c4d06b6fdec0055feab7ade
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2025-02-23 10:47:12 +0100

    feat(farcaster): add frame

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, useEffect, ReactNode, JSX } from 'react';
+import { createRoot } from 'react-dom/client';
+import FrameSDK from '@farcaster/frame-sdk';
+import './index.css';
+import App from './App.tsx';
+
+function FarcasterFrameProvider({ children }: { children: ReactNode }): JSX.Element {
+  useEffect((): void => {
+    const init = async () => {
+      setTimeout((): void => {
+        FrameSDK.actions.ready();
+      }, 500);
+    };
+    init();
+  }, []);
+  return <>{children}</>;
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FarcasterFrameProvider>
+      <App />
+    </FarcasterFrameProvider>
   </StrictMode>,
-)
+);
